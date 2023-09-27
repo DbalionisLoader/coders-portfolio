02a0745561ec52d5ba641231bcdd3d7c67525d0e
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-09-27 15:33:40 +0100

    Fix issue with site.json not being reached

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css/style.css");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("images/png");
+    eleventyConfig.addPassthroughCopy("site.json");
     /* Custom Filters */
     eleventyConfig.addCollection("examplePosts", function(collection) {
         return collection.getFilteredByGlob("./posts/examples/*.md");
